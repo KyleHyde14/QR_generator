a96e32cd305f26d15910c38759a709e4340f1e90
KyleHyde14 <jesushdezjob@gmail.com>
2024-05-13 21:50:58 +0200

    deleted unused function

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
 from views import views
-from delete_QR import execute_delete
-import threading
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix='/')
-
-delete_thread = threading.Thread(target=execute_delete, daemon=True)
-delete_thread.start()
 
 if __name__ == '__main__':
     app.run(debug=False)
